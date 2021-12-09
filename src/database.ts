56e42bcd21f64d1cceb37f627f5b1969767b17c5
icocampos <icarocampos97@gmail.com>
2021-12-09 02:30:22 -0300

    add dotenv

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,9 @@
 import { Pool } from 'pg';
 
 export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'ic2150',
-    database: 'tarefasapi',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     port: 5432
 });
