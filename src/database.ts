63ca7584be0850f3da291d1466947e3555bfce8d
icocampos <icarocampos97@gmail.com>
2021-12-09 13:05:43 -0300

    upd pass

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,7 @@ import { Pool } from 'pg';
 export const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    password: 'ic2150',
+    password: 'postgres',
     database: 'tarefasapi',
     port: 5432
 });
